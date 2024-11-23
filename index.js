8d98dc84a312e375793a8b6547102c41f50ddfc9
Zainudin, Ahmad Fuad <afuad.zai@gmail.com>
2024-11-23 13:21:46 +0800

    Add home page template

--- index.js
+++ index.js
@@ -8,10 +8,52 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
-app.get("/", (req, res)=>{
-    res.render("home.ejs");
+app.get("/", (req, res) => {
+  res.render("home.ejs", { blogs: blogs });
 });
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+const formatter = new Intl.DateTimeFormat("en-us", {
+  month: "short",
+  year: "numeric",
+});
+
+const blogs = [
+  {
+    title: "One Step Forward",
+    subtitle:
+      "Accepting the reality; your skill sets are not in demand right now, and it's time for new skills.",
+    date: formatter.format(new Date()),
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Stuck in a Rut",
+    subtitle:
+      "What is it like losing your income, but your rent and utilities still goes on.",
+    date: formatter.format(new Date(2024, 3)),
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "4 Years in Fortune 500 Company",
+    subtitle: "It's good money, but is it happy soul?",
+    date: formatter.format(new Date(2023, 7)),
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "3 Years in Fortune 500 Company",
+    subtitle:
+      "The higher you go, the heavier your responsibilities. You wonder, how long can you carry this load?",
+    date: formatter.format(new Date(2022, 7)),
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "2 Years in Fortune 500 Company",
+    subtitle:
+      "Finally you able to catch your breath, but you know your respite is temporary.",
+    date: formatter.format(new Date(2021, 7)),
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+];
